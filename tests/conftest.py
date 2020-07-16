c038dac6b3de7b9dbee5ee33c51bc4c0e8d52e63
Christian Barra <me@christianbarra.com>
2020-07-16 15:12:48 +0200

    Add token fixture and change client scope

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,21 @@ from falcon import testing
 from paymentify.main import api
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def client():
     return testing.TestClient(api)
+
+
+@pytest.fixture
+def token():
+    class MockedToken:
+        id = "tok_1H5VCmLc6lT5RCtGLvo2LSmC"
+        created = 1594898688
+
+        class card:
+            id = "card_1H5VCmLc6lT5RCtGSWahXZPJ"
+            exp_month = 7
+            exp_year = 21
+            last4 = 4242
+
+    return MockedToken
