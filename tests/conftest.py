da086cbe682ec7944828d1d2d82399a09bfade8d
Christian Barra <me@christianbarra.com>
2020-07-19 01:28:05 +0200

    Add new fixture for charge and tests for sale endpoint

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ def client():
     return testing.TestClient(api)
 
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def token():
     class MockedToken:
         id = "tok_1H5VCmLc6lT5RCtGLvo2LSmC"
@@ -22,3 +22,16 @@ def token():
             last4 = 4242
 
     return MockedToken
+
+
+@pytest.fixture(scope="module")
+def charge():
+    class MockedCharge:
+        id = "ch_1H6HA0Lc6lT5RCtGK5QMjc8S"
+        amount = 2000
+        payment_method = "card_1H6C5eLc6lT5RCtGc9WIqjc5"
+        paid = True
+        status = "succeeded"
+        created = 1594898688
+
+    return MockedCharge
--- tests/test_api_sale.py
+++ tests/test_api_sale.py
@@ -1,7 +1,50 @@
+import json
+from unittest import mock
+
 import falcon
+from hypothesis import given
+from hypothesis import strategies as st
 
 
 def test_get_sale_not_allowed(client):
     response = client.simulate_get("/sale")
 
     assert response.status == falcon.HTTP_405
+
+
+@given(
+    st.text(min_size=5), st.integers(min_value=1),
+)
+def test_post_sale_valid_body(client, charge, token, amount):
+    body = {
+        "token": token,
+        "amount": amount,
+    }
+
+    with mock.patch("stripe.Charge.create") as mock_response:
+        mock_response.return_value = charge
+        response = client.simulate_post("/sale", json=body)
+
+    assert json.loads(response.content) == {
+        "id": charge.id,
+        "amount": charge.amount,
+        "payment_method": charge.payment_method,
+        "paid": charge.paid,
+        "status": charge.status,
+        "created": charge.created,
+    }
+    assert response.status == falcon.HTTP_201
+
+
+@given(st.text(max_size=4), st.floats())
+def test_post_sale_invalid_body(client, charge, token, amount):
+    body = {
+        "token": token,
+        "amount": amount,
+    }
+
+    with mock.patch("stripe.Charge.create") as mock_response:
+        response = client.simulate_post("/sale", json=body)
+
+    mock_response.assert_not_called()
+    assert response.status == falcon.HTTP_BAD_REQUEST
